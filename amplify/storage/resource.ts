462ff9b94ccd0d8495202d05ab42deae7f83032b
sutariachintan <sutariachintan@gmail.com>
2024-08-20 10:58:06 -0500

    adding storage permission

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,3 +3,13 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'amplifyTeamDrive'
 });
+
+export const storage = defineStorage({
+  name: 'amplifyTeamDrive',
+  access: (allow) => ({
+    'picture-submissions/*': [
+      allow.authenticated.to(['read','write']),
+      allow.guest.to(['read', 'write'])
+    ],
+  })
+});
